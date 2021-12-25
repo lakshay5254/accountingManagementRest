95985ad89d5e84fb767182402609a47bf6a47cc4
lakshay <lakshay5254@gmail.com>
2021-12-25 14:18:12 +0530

    Accounting Management Rest

---
+++ src/main/java/com/lakshay/accountingManagementRest/AccountingManagementRestApplication.java
@@ -0,0 +1,13 @@
+package com.lakshay.accountingManagementRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingManagementRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountingManagementRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lakshay/accountingManagementRest/AccountingManagementRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lakshay.accountingManagementRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingManagementRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
